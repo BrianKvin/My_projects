44deb65ff49e6b1cdc6f87487a5bc7862fc60bb9
Bkelvin <briankvin@gmail.com>
2023-04-13 07:44:09 +0300

    HTML, CSS, JavaScript

--- Login_form_design/index.js
+++ Login_form_design/index.js
@@ -2,10 +2,17 @@ const Loginform = document.querySelector(".Loginform");
 const MainButton = document.querySelector(".MainButton");
 const Registerform = document.querySelector(".Registerform");
 const Registerbtn = document.querySelector(".Registerbtn");
+const Loginbtn = document.querySelector(".Loginbtn");
+
 MainButton.addEventListener("click", () => {
     Loginform.classList.add("Loginform-box");
+    Registerform.classList.remove("Registerform-box");
 });
 Registerbtn.addEventListener("click", () => {
     Registerform.classList.add("Registerform-box");
     Loginform.classList.remove("Loginform-box");
+});
+Loginbtn.addEventListener("click", () => {
+    Loginform.classList.add("Loginform-box");
+    Registerform.classList.remove("Registerform-box");
 });
