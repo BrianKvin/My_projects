f9186b283fee3501050e2e4a5983972715606d59
Bkelvin <briankvin@gmail.com>
2023-04-26 15:57:58 +0300

    HTML, CSS, Bootstrap, JavaScript, Python-Flask

---
+++ Python_projects/Flask_blog/flask_blog.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Brian Kelvin',
+        'title': 'Blog Post 1',
+        'content': 'First post conte',
+        'date_posted': 'April 20, 2023'
+    },
+    {
+        'author': 'Vincente Cecil',
+        'title': 'Blog Post 2',
+        'content': 'Second post conte',
+        'date_posted': 'April 22, 2023'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
