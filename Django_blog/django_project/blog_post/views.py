33209c0ccaed2ef244fe6d98e96378bbc2bb4a8f
BrianKvin <briankvin@gmail.com>
2023-07-31 10:53:22 +0300

    New Progress

--- Django_blog/django_project/blog_post/views.py
+++ Django_blog/django_project/blog_post/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Kelvin Brian',
-        'title': 'Blog Post 1',
-        'content': 'First Blog Post',
-        'date_posted': 'July 20/2023'
-    },
-    {
-        'author': 'Spencer Onyango',
-        'title': 'Blog Post 2',
-        'content': 'Second Blog Post',
-        'date_posted': 'July 25/2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all
     }
     return render(request, 'blog/home.html', context)
 
